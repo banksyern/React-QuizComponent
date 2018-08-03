6f768807eb2af391ea816d175ad2d9d4b2842c67
banksyern <42044951+banksyern@users.noreply.github.com>
2018-08-03 10:56:05 -0500

    ss

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,14 +5,16 @@ let quizData = require('./quiz_data.json');
 class Quiz extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {quiz_position: 1};
 
   }
 
   render() {
     return (
     <div>
-      Quiz
+      <div className="QuizQuestion">
+        {quizData.quiz_questions.instruction_text}
+      </div>
     </div>
     )
   }
