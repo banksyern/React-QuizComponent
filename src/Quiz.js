cd009f426d433f22895a38a35262febc9e5470d2
banksyern <42044951+banksyern@users.noreply.github.com>
2018-08-03 10:36:50 -0500

    asdf

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,1 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+    
+
+  }
+
+  render() {
+    <div>
+      Quiz
+    </div>
+  }
+
+}
+
+export default Quiz;
