951cfd2aa71e44fed10f1bd6ac841807b57f6639
banksyern <42044951+banksyern@users.noreply.github.com>
2018-08-09 13:16:19 -0500

    5

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,12 @@ class Quiz extends Component {
     };
   }
 
+  showNextQuestion() {
+    this.setState({
+        quiz_position: this.state.quiz_position + 1
+    });
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length ? true : false;
     return (
@@ -19,7 +25,7 @@ class Quiz extends Component {
         isQuizEnd ? (
           <QuizEnd/>
         ) : (
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}></QuizQuestion>
+          <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}></QuizQuestion>
         )
       }
     </div>
